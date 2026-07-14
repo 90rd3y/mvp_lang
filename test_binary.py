b576556c38bee6df9e68592f2d4b2216a800aec2
Гордей Пузанов <gordey.puzanov@gmail.com>
2026-07-14 09:49:54 +0500

    Рефакторинг: очистка корневой директории и объединение документации в docs/

--- test_binary.py
+++
@@ -1,3 +0,0 @@
-with open("src/Semantic.cpp", "r") as f: c = f.read()
-c = c.replace("Lexer::TokenType op = node.token.type;", "Lexer::TokenType op = node.token.type;\n    std::cout << \"DEBUG BINARY OP: \" << (int)op << std::endl;")
-with open("src/Semantic.cpp", "w") as f: f.write(c)
