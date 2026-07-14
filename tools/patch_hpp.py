020d801bd660817e21724c0c707a0b52c39bfe77
Гордей Пузанов <gordey.puzanov@gmail.com>
2026-07-14 21:30:21 +0500

    final doc edits

--- tools/patch_hpp.py
+++
@@ -1,4 +0,0 @@
-import re
-with open("inc/Interpreter.hpp", "r") as f: content = f.read()
-content = content.replace("std::unordered_map<Lexer::IdentId, Parser::NodeId> functions;", "std::unordered_map<Lexer::IdentId, Parser::NodeId> functions;\n  std::string namespace_prefix = \"\";")
-with open("inc/Interpreter.hpp", "w") as f: f.write(content)
--- tools/test.py
+++
@@ -1,4 +0,0 @@
-import re
-with open("src/Interpreter.cpp") as f: c = f.read()
-if "case Parser::NodeType::ArrayLiteral:" in c: print("ArrayLiteral is in Interpreter.cpp")
-else: print("ArrayLiteral is MISSING")
